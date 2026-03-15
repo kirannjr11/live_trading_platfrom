383737118e240a551b787aadf21a9e125284f418
kirannjr11 <ghalekiran03@gmail.com>
2026-03-15 14:00:43 +0900

    created pricing in Home

--- src/landing_page/home/Pricing.js
+++ src/landing_page/home/Pricing.js
@@ -1,8 +1,32 @@
-import React from 'react'
+import React from "react";
 function Pricing() {
-    return ( 
-        <p>pricing</p>
-     );
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-4">
+          <h1>Unbeatable pricing</h1>
+          <p>
+            We pioneered the concept of discount booking and price transparancy
+            in Nepal. Flat fees and no hidden chargges
+          </p>
+          <a href=""></a>
+        </div>
+        <div className="col-2"></div>
+        <div className="col-6">
+          <div className="row text-center">
+            <div className="col p-2 border">
+              <h1 className="mb-3">Rs 0</h1>
+              <p>Free equity delivery and direct mutual funds</p>
+            </div>
+            <div className="col p-2 border">
+              <h1 className="mb-3">Rs 20</h1>
+              <p>Free equity delivery and direct mutual funds</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Pricing;
---
+++ src/landing_page/pricing/Brokerage.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+function Brokerage() {
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-4">
+          <h1>Unbeatable pricing</h1>
+          <p>
+            We pioneered the concept of discount booking and price transparancy
+            in Nepal. Flat fees and no hidden chargges
+          </p>
+          <a href=""></a>
+        </div>
+        <div className="col-2"></div>
+        <div className="col-6"></div>
+      </div>
+    </div>
+  );
+}
+
+export default Brokerage;
